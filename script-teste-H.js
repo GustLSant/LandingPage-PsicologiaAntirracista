d1e8cc354d79e9db3179456b9d6acd4b190bf32e
Gustavo LS <93957341+GustLSant@users.noreply.github.com>
2023-11-02 00:22:56 -0300

    uso do screen.height

--- script-teste-H.js
+++ script-teste-H.js
@@ -3,7 +3,7 @@ document.getElementById('innerH').textContent = window.innerHeight
 printarAltura()
 
 async function printarAltura(){
-    document.getElementById('innerH').textContent = window.innerHeight
+    document.getElementById('innerH').textContent = window.screen.height;
     document.getElementById('outerH').textContent = window.outerHeight
     document.getElementById('contador').textContent = Math.random()
     setTimeout(printarAltura, 500)
